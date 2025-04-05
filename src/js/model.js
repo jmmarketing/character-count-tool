b61eb3fddd7162784c1787191582f6c734c7c34a
jmmarketing <jeff@jeffreymclean.com>
2025-04-05 12:33:13 -0700

    Started mental mapping of model and data logic

---
+++ src/js/model.js
@@ -0,0 +1,16 @@
+//Psuedo code:
+/* What calculations and states need to be recognized and done:
+- Is Exclude Spaces checked?
+- Is there a Chracter limit? 
+    -- Both of these could be passed as parameters with default set to false. 
+
+- Total input (string) needs to be recieved. 
+- Length = Characters (+/- spaces)
+- Spaces = Word Count
+- Periods = Sentence count. 
+- Letter Density
+    - Reduce over string and increase count based on letter (MDN has example i think). 
+
+Out put should be one data object. 
+
+*/
