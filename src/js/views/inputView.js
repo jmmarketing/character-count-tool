e66fad532ec017c19edb1d17093722882ef00219
jmmarketing <jeff@jeffreymclean.com>
2025-04-05 14:22:27 -0700

    Working on logic of showing/hiding character limit and storing number. Using an _inputData object to track input options. Will be returned in other function called by controller and sent to model for stats logic.

--- src/js/views/inputView.js
+++ src/js/views/inputView.js
@@ -4,6 +4,12 @@ class InputView {
   _characterOption = document.querySelector("#limit");
   _limit = document.querySelector("#limit-count");
 
+  _inputData = {
+    input: null,
+    spaces: false,
+    limit: null,
+  };
+
   constructor() {
     this._init();
   }
@@ -13,12 +19,27 @@ class InputView {
       "change",
       this._toggleCharacterLimit.bind(this)
     );
+
+    this._limit.addEventListener("change", this._setCharacterLimit.bind(this));
   }
 
   _toggleCharacterLimit() {
-    this._characterOption.checked
-      ? this._limit.classList.remove("hide")
-      : this._limit.classList.add("hide");
+    if (this._characterOption.checked) {
+      this._limit.classList.remove("hide");
+    } else {
+      this._limit.classList.add("hide");
+      this._inputData.limit = null;
+
+      this._limit.value = "";
+    }
+
+    console.log(this._inputData);
+  }
+
+  _setCharacterLimit() {
+    const characterLimit = this._limit.valueAsNumber;
+    characterLimit !== NaN && (this._inputData.limit = characterLimit);
+    console.log(this._inputData);
   }
 }
 
